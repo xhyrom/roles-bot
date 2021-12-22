a414b11651f2a51eef6b05dc8d8c51ece53f70d4
xhyrom <generalkubo@gmail.com>
2021-12-22 19:05:07 +0100

    page fix

--- src/web/pages/index.tsx
+++ src/web/pages/index.tsx
@@ -1,9 +1,12 @@
 import Head from "next/head";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <div>
       <Head>
+        <title>Roles Bot</title>
+        <link rel="icon" href="logo.ico" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/default.min.css" />
       </Head>
       <section className="flex-container">
@@ -15,17 +18,16 @@ export default function Home() {
             </form>
             <button id="addRole">Add Role</button>
             <button id="buttonCopy">Copy</button>
-            <img src="slash.png" className="responsive" />
             <pre className={`hljs language-json copy`} id="jsonPre"><code id="json" className="code"></code></pre>
         </div>
       </section>
 
-      <script src="https://kit.fontawesome.com/5acf4d9e80.js" crossOrigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-      <script src="script.js"></script>
-      <script>hljs.initHighlightingOnLoad();</script>
+      <Script src="https://kit.fontawesome.com/5acf4d9e80.js" crossOrigin="anonymous"></Script>
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></Script>
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"></Script>
+      <Script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></Script>
+      <Script src="script.js"></Script>
+      <Script id='hljs'>hljs.initHighlightingOnLoad();</Script>
     </div>
   )
 }
