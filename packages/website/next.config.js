65b7c2f4c2f080e31f01226337ce549e0199ff0f
xhyrom <generalkubo@gmail.com>
2022-01-02 19:01:45 +0100

    secure headers

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -1,3 +1,14 @@
+const { createSecureHeaders } = require("next-secure-headers");
+
 module.exports = {
-    assetPrefix: '/roles-bot/'
+    assetPrefix: '/roles-bot/',
+    async headers() {
+        return [{
+          source: "/(.*)",
+          headers: createSecureHeaders({
+            forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true }],
+            referrerPolicy: "same-origin",
+          }),
+        }];
+    },
 }
