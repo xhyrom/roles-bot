c7b55c9c06265193fda136e81ed3be52df8dd722
xhyrom <generalkubo@gmail.com>
2022-01-02 19:02:40 +0100

    refactor(*): eslint

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -1,14 +1,14 @@
-const { createSecureHeaders } = require("next-secure-headers");
+const { createSecureHeaders } = require('next-secure-headers');
 
 module.exports = {
-    assetPrefix: '/roles-bot/',
-    async headers() {
-        return [{
-          source: "/(.*)",
-          headers: createSecureHeaders({
-            forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true }],
-            referrerPolicy: "same-origin",
-          }),
-        }];
-    },
-}
+	assetPrefix: '/roles-bot/',
+	async headers() {
+		return [{
+			source: '/(.*)',
+			headers: createSecureHeaders({
+				forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true }],
+				referrerPolicy: 'same-origin',
+			}),
+		}];
+	},
+};
