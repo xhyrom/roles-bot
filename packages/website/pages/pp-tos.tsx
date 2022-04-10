6ec3bb47b3b593e4565f895038a498642242822c
xhyrom <generalkubo@gmail.com>
2022-04-10 21:31:02 +0200

    privacy policy & tos

---
+++ packages/website/pages/pp-tos.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Roles Bot</title>
+				<link rel="icon" href="logo.ico" />
+			</Head>
+
+			<section>
+				<h1>Roles Bot Privacy Policy</h1>
+                <h3>Last updated: April 10, 2022</h3>
+                <hr />
+
+                Roles Bot does not collect any informations.
+			</section>
+            <br/>
+			<section>
+				<h1>Roles Bot Terms Of Services</h1>
+                <h3>Last updated: April 10, 2022</h3>
+                <hr />
+
+                Don't use the bot for bad intentions
+			</section>
+		</div>
+	);
+}
