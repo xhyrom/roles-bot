f22e1d64300466467b08cb85741e189be962384b
xHyroM <generalkubo@gmail.com>
2023-04-03 17:04:16 +0200

    feat: add website

---
+++ packages/website/.prettierrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    plugins: [
+      require.resolve("prettier-plugin-astro"),
+      require.resolve("prettier-plugin-tailwindcss"),
+    ],
+    overrides: [
+      {
+        files: "*.astro",
+        options: {
+          parser: "astro",
+        },
+      },
+    ],
+  };
+  
