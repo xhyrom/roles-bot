c10de80d184354ad70e6d5a37596db559058cc7c
OLIMINATOR <oliminator@protonmail.com>
2023-04-08 14:18:18 +0200

    feat(website): change invite button color

--- packages/website/src/config.ts
+++ packages/website/src/config.ts
@@ -1,6 +1,7 @@
 export const CONFIG = {
 	name: "Roles Bot",
-	description: "soon",
-	themeColor: "#2F5AF3",
+	description:
+		"Ultimate solution for managing reaction roles in your Discord server, and the best part is, it's completely free! No hidden charges or payments like other bots. Try it out today and see for yourself how easy it is to use!",
+	themeColor: "#fbc11a",
 	origin: "https://roles-bot.xhyrom.dev",
 };
