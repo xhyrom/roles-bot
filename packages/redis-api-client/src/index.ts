3cd6065ab896fc080d7899de845fdaaff5765c9c
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-12-14 21:51:14 +0100

    feat(redis api client): improve

--- packages/redis-api-client/src/index.ts
+++ packages/redis-api-client/src/index.ts
@@ -1,72 +1,136 @@
-export class RedisAPIClient {
-	private apiKey: string;
-	private host: string;
+interface RedisClient {
+  connect(): Promise<void> | void;
 
-	constructor(apiKey: string, host: string) {
-		this.apiKey = apiKey;
-		this.host = host;
-	}
+  get(key: string): Promise<string | null>;
+  set(key: string, value: string): Promise<string>;
+  setex(key: string, value: string, seconds: number): Promise<string>;
+  del(key: string): Promise<number>;
+}
 
-	public async get(key: string): Promise<string | null> {
-		const url = `${this.host}/get?key=${key}`;
-		const response = await fetch(url, {
-			headers: {
-				Authorization: this.apiKey,
-			},
-		});
+export class Upstash implements RedisClient {
+  private url: string;
+  private token: string;
 
-		const text = await response.text();
-		return text === "null" ? null : text;
-	}
+  private redis: import("@upstash/redis").Redis | undefined;
 
-	public async set(key: string, value: string): Promise<string> {
-		const url = `${this.host}/set`;
-		const response = await fetch(url, {
-			method: "POST",
-			headers: {
-				Authorization: this.apiKey,
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				key,
-				value,
-			}),
-		});
+  constructor(url: string, token: string) {
+    this.url = url;
+    this.token = token;
 
-		return response.text();
-	}
+    this.redis = undefined;
+  }
 
-	public async setex(
-		key: string,
-		value: string,
-		seconds: number,
-	): Promise<string> {
-		const url = `${this.host}/setex`;
-		const response = await fetch(url, {
-			method: "POST",
-			headers: {
-				Authorization: this.apiKey,
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				key,
-				value,
-				seconds,
-			}),
-		});
+  public async connect() {
+    if (this.redis) {
+      return;
+    }
 
-		return response.text();
-	}
+    this.redis = new (await import("@upstash/redis")).Redis({
+      url: this.url,
+      token: this.token,
+    });
+  }
 
-	public async del(key: string): Promise<string> {
-		const url = `${this.host}/del?key=${key}`;
-		const response = await fetch(url, {
-			method: "DELETE",
-			headers: {
-				Authorization: this.apiKey,
-			},
-		});
+  public async get(key: string): Promise<string | null> {
+    await this.connect();
 
-		return response.text();
-	}
+    return (await this.redis?.get(key)) ?? null;
+  }
+
+  public async set(key: string, value: string): Promise<string> {
+    await this.connect();
+
+    return (await this.redis?.set(key, value)) ?? "OK";
+  }
+
+  public async setex(
+    key: string,
+    value: string,
+    seconds: number
+  ): Promise<string> {
+    await this.connect();
+
+    return (await this.redis?.setex(key, seconds, value)) ?? "OK";
+  }
+
+  public async del(key: string): Promise<number> {
+    await this.connect();
+
+    return (await this.redis?.del(key)) ?? 0;
+  }
+}
+
+export class Raw implements RedisClient {
+  private apiKey: string;
+  private host: string;
+
+  constructor(apiKey: string, host: string) {
+    this.apiKey = apiKey;
+    this.host = host;
+  }
+
+  public connect() {}
+
+  public async get(key: string): Promise<string | null> {
+    const url = `${this.host}/get?key=${key}`;
+    const response = await fetch(url, {
+      headers: {
+        Authorization: this.apiKey,
+      },
+    });
+
+    const text = await response.text();
+    return text === "null" ? null : text;
+  }
+
+  public async set(key: string, value: string): Promise<string> {
+    const url = `${this.host}/set`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: this.apiKey,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        key,
+        value,
+      }),
+    });
+
+    return response.text();
+  }
+
+  public async setex(
+    key: string,
+    value: string,
+    seconds: number
+  ): Promise<string> {
+    const url = `${this.host}/setex`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: this.apiKey,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        key,
+        value,
+        seconds,
+      }),
+    });
+
+    return response.text();
+  }
+
+  public async del(key: string): Promise<number> {
+    const url = `${this.host}/del?key=${key}`;
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        Authorization: this.apiKey,
+      },
+    });
+
+    return parseInt(await response.text());
+  }
 }
